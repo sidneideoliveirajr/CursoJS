9a8f82de3840aa34f2ab9e925abe396963a719e1
sidneideoliveirajr <75648174+sidneideoliveirajr@users.noreply.github.com>
2021-02-17 19:59:22 -0300

    updaate sj_aula_Repetições_17/02/2021

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+console.log('\nUtilizando do while na Repetição ')
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6);
+
+
+
+/* função while com teste lógico no início
+while (c <= 3){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
